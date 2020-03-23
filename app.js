d8dcb6da8c408dd1cdc32a262e3d3f7701e786ab
Ernest Knightstar Gamboa <gamboae@ecs.csus.edu>
2020-03-23 00:34:14 -0700

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const expresslayouts = require('express-ejs-layouts')
 
+const indexRouter = require('./routes/index')
 
 app.set('view engine','ejs')
 
@@ -10,5 +11,8 @@ app.set('layout','layouts/layout')
 app.use(expresslayouts)
 app.use(express.static('public'))
 
+const mongoose = require('mongoose')
 
-app.listen(procces.env.PORT || 3000)
+
+app.use('/',indexRouter)
+app.listen(process.env.PORT || 3000)
